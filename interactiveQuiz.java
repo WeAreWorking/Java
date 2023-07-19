53084fff351bb7c0a1f5efe07a851737fec3359f
WeAreWorking <133279246+WeAreWorking@users.noreply.github.com>
2023-07-18 17:10:46 -0700

    idk

---
+++ interactiveQuiz.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class interactiveQuiz {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println();
+        var yup = "";
+        System.in.wait(2000);
+    }
+}
